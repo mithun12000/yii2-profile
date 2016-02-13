2388973a8deef4dd4cc5c2600807cf942ec7a064
Mithun Mandal <mithun12000@gmail.com>
2016-02-13 15:59:23 +0530

    added profile module

---
+++ src/Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace mithun\profile;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'mithun\profile\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
